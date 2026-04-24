7a000d530919ab019115ae6cd9b16ab4fcdaad8f
bnanwani <bnanwani@pm.me>
2026-04-23 22:58:07 -0700

    Initial nvim config

---
+++ nvim/.config/nvim/lua/config/options.lua
@@ -0,0 +1,12 @@
+vim.g.have_nerd_font = true
+vim.o.number = true
+vim.o.mouse = 'a'
+vim.schedule(function() vim.o.clipboard = 'unnamedplus' end)
+vim.o.ignorecase = true
+vim.o.smartcase = true
+vim.o.signcolumn = 'yes'
+vim.o.updatetime = 250
+vim.o.timeoutlen = 300
+vim.o.splitright = true
+vim.o.splitbelow = true
+vim.o.cursorline = true
